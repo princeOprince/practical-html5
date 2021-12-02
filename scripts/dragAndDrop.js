96e8e934b3ceff3005cf1f8b3a30b60acc583572
Prince Odia <princeodia@gmail.com>
2021-12-02 12:41:53 +0100

    Use web storage options

--- scripts/dragAndDrop.js
+++ scripts/dragAndDrop.js
@@ -1,13 +1,50 @@
 function addDnDHandlers() {
   const coffeeImages = document.getElementsByClassName('productarticlewide');
   const shoppingCartDropZone = document.getElementById('shoppingcart');
-  const shoppingcart = document.querySelectorAll('#shoppingcart ul')[0];
+  const shoppingcart = document.querySelector('#shoppingcart ul');
+
+  class Coffee {
+    constructor(id, price) {
+      this.coffeeId = id;
+      this.price = price;
+    }
+  }
 
   for (let i = 0; i < coffeeImages.length; i++) {
     coffeeImages[i].addEventListener('dragstart', function(event) {
       event.dataTransfer.effectAllowed = "copy";
       event.dataTransfer.setData('text', this.getAttribute('id'));
     });
+  }
+
+  class Cart {
+    coffees = [];
+  }
+  
+  let currentCart = null;
+  currentCart = JSON.parse(localStorage.getItem('cart'));
+  if (!currentCart)
+    createEmptyCart();
+  updateShoppingCartUI();
+
+  function createEmptyCart() {
+    localStorage.clear();
+    localStorage.setItem('cart', JSON.stringify(new Cart()));
+    currentCart = JSON.parse(localStorage.getItem('cart'));
+  }
+
+  function updateShoppingCartUI() {
+    shoppingcart.innerHTML = "";
+    for (let i = 0; i < currentCart.coffees.length; i++) {
+      const liElement = document.createElement('li');
+      liElement.textContent = currentCart.coffees[i].coffeeId + " " + currentCart.coffees[i].price;
+      shoppingcart.appendChild(liElement);
+    }
+  }
+
+  currentCart.addCoffee = (coffee) => {
+    currentCart.coffees.push(coffee);
+    localStorage.setItem('cart', JSON.stringify(currentCart));
   }
 
   shoppingCartDropZone.addEventListener('dragover', (event) => {
@@ -22,9 +59,9 @@ function addDnDHandlers() {
   });
 
   function addCoffeeToShoppingCart(item, id) {
-    const html = id + " " + item.dataset.price;
-    const liElement = document.createElement('li');
-    liElement.innerHTML = html;
-    shoppingcart.appendChild(liElement);
+    const price = item.dataset.price;
+    const coffee = new Coffee(id, price);
+    currentCart.addCoffee(coffee);
+    updateShoppingCartUI();
   }
 }
